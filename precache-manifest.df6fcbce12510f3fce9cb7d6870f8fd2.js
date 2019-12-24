22c96683142e9f8ac0b3660d9f2ae8a41adccae4
Bojan Krstic <krstic.bojan80@gmail.com>
2019-12-24 21:27:21 +0100

    Updates

---
+++ precache-manifest.df6fcbce12510f3fce9cb7d6870f8fd2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7654aaf267acdf52bc9",
+    "url": "/forecast-serbia/static/css/main.f53aa789.chunk.css"
+  },
+  {
+    "revision": "f7654aaf267acdf52bc9",
+    "url": "/forecast-serbia/static/js/main.f7654aaf.chunk.js"
+  },
+  {
+    "revision": "711729df6b62ce348b72",
+    "url": "/forecast-serbia/static/js/runtime~main.711729df.js"
+  },
+  {
+    "revision": "5d9f59297f45bd55a81f",
+    "url": "/forecast-serbia/static/js/2.5d9f5929.chunk.js"
+  },
+  {
+    "revision": "492aef1167956247c2151bfb19b5a85f",
+    "url": "/forecast-serbia/static/media/snow.492aef11.svg"
+  },
+  {
+    "revision": "82ff30057a21cc6bc339bbc78516a77e",
+    "url": "/forecast-serbia/static/media/serbia.82ff3005.png"
+  },
+  {
+    "revision": "4b42731b96810f66246577702e146675",
+    "url": "/forecast-serbia/static/media/clear-day.4b42731b.svg"
+  },
+  {
+    "revision": "83043161160323991204bae6ed073c71",
+    "url": "/forecast-serbia/static/media/partly-cloudy-day.83043161.svg"
+  },
+  {
+    "revision": "8c244a470a9332a9f7b78845e5d53762",
+    "url": "/forecast-serbia/static/media/clear-night.8c244a47.svg"
+  },
+  {
+    "revision": "3388310388a81d4f3f48c11e1de5340e",
+    "url": "/forecast-serbia/static/media/cloudy.33883103.svg"
+  },
+  {
+    "revision": "73cce30d2098b0aa1e312573b72bba27",
+    "url": "/forecast-serbia/static/media/partly-cloudy-night.73cce30d.svg"
+  },
+  {
+    "revision": "01bcb6c9da95a49e417d53dea1d11fae",
+    "url": "/forecast-serbia/static/media/fog.01bcb6c9.svg"
+  },
+  {
+    "revision": "5d44f04041c840f57cbc4f40b62bd77b",
+    "url": "/forecast-serbia/static/media/rain.5d44f040.svg"
+  },
+  {
+    "revision": "a0d9e9ae61e939065711a5f23ed55329",
+    "url": "/forecast-serbia/static/media/sleet.a0d9e9ae.svg"
+  },
+  {
+    "revision": "4a70a5060d7937b5d3ec70dd9f343c8e",
+    "url": "/forecast-serbia/index.html"
+  }
+];
